a5d25482017c131c2904972c7aabadaeb921e46e
Wilson Sgroi <wilson.sgroi@keystone.local>
2025-06-13 15:51:31 +0200

    msalConfig.js

--- frontend/src/msalConfig.js
+++ frontend/src/msalConfig.js
@@ -1,14 +1,10 @@
 // src/msalConfig.js
 export const msalConfig = {
   auth: {
-    // Application (client) ID dell’app registrata in Entra ID
-    clientId: "56998f02-856b-495e-8778-884036e74381",
-
-    // Issuer del tuo tenant (single-tenant)
-    authority: "https://login.microsoftonline.com/3630ff06-11ea-4763-960a-fc74e8780220",
-
-    // Lascia “/” → Static Web Apps aggiunge in automatico il dominio
+    clientId: "95622910-72b1-4eeb-98ea-fa20efbc5673",               // Application (client) ID
+    authority:
+      "https://login.microsoftonline.com/3630ff06-11ea-4763-960a-fc74e8780220", // Tenant ID
     redirectUri: "https://mango-ocean-06166b203.6.azurestaticapps.net/"
   },
-  cache: { cacheLocation: "sessionStorage" }   // default ok
+  cache: { cacheLocation: "sessionStorage" }
 };
