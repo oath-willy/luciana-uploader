af436bc20e54a40f95df522bb008e905b623fdd1
Wilson Sgroi <wilson.sgroi@keystone.local>
2025-06-17 15:27:55 +0200

    fix

--- src/auth/msalConfig.ts
+++ src/auth/msalConfig.ts
@@ -15,27 +15,3 @@ export const msalConfig: Configuration = {
 export const loginRequest = {
   scopes: ["https://storage.azure.com/user_impersonation"],
 };
-
-// src/pages/LoginPage.tsx
-import { useMsal } from "@azure/msal-react";
-import { Button, Container, Title } from "@mantine/core";
-import { loginRequest } from "../auth/msalConfig";
-
-export const LoginPage = () => {
-  const { instance } = useMsal();
-
-  const handleLogin = () => {
-    instance.loginRedirect(loginRequest);
-  };
-
-  return (
-    <Container size="sm" style={{ marginTop: 100 }}>
-      <Title align="center" order={2} mb="lg">
-        Benvenuto nel progetto Luciana
-      </Title>
-      <Button fullWidth size="md" onClick={handleLogin}>
-        Accedi con account aziendale
-      </Button>
-    </Container>
-  );
-};
