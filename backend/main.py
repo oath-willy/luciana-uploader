9c45c9521b76a7e9f8e52dd804017888ff971534
Wilson Sgroi <wilson.sgroi@keystone.local>
2025-07-09 12:22:43 +0200

    backend

--- backend/main.py
+++ backend/main.py
@@ -8,10 +8,7 @@ app = FastAPI()
 # CORS: permetti al frontend (localhost:3000) di parlare col backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://yellow-forest-0ad79d503.6.azurestaticapps.net"
-        ],  # URL del frontend
+    allow_origins=["*"],  # URL del frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -21,7 +18,3 @@ app.add_middleware(
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
