b73bf27b40192c30abe613712a1a462f07dc2f39
Wilson Sgroi <wilson.sgroi@keystone.local>
2025-07-09 12:05:30 +0200

    backend

--- backend/main.py
+++ backend/main.py
@@ -21,3 +21,7 @@ app.add_middleware(
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
