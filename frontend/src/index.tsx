c475fe35082250d89f1586b554b31c3ef8dcc5be
Wilson Sgroi <wilson.sgroi@keystone.local>
2025-07-17 17:22:50 +0200

    explorer 21

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
+    <MantineProvider>
       <App />
     </MantineProvider>
   </React.StrictMode>
