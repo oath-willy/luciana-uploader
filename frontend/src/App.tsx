0b962e991c51380b2592eaf56ec4a13b888c2baa
Wilson Sgroi <wilson.sgroi@keystone.local>
2025-07-23 15:39:38 +0200

    admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { createTheme } from '@mantine/core';
 
 //Components
 import { RequireAuth } from "./components/RequireAuth";
