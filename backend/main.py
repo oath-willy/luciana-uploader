2e95a8941aff42161525b5e47d584214e6b05ac4
Wilson Sgroi <wilson.sgroi@keystone.local>
2025-12-04 15:43:36 +0100

    main.py fix
    main.py fix

--- backend/main.py
+++ backend/main.py
@@ -4,15 +4,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import HTTPException
 from dotenv import load_dotenv
+
 from api.routes import router
 from api import run_script
 from api import run_script_log
-
 from api import products
-app.include_router(products.router, prefix="/api")
-
 
 load_dotenv()
+
 app = FastAPI()
 
 frontend_origin = os.getenv("FRONTEND_ORIGIN", "http://localhost:3000")
@@ -25,14 +24,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Healthcheck
 @app.get("/ping")
 def ping():
     return {"message": "Hello!"}
 
-# Include tutte le rotte
+# Include i router DOPO la creazione dell'app
+app.include_router(products.router, prefix="/api")
 app.include_router(router, prefix="/api")
 app.include_router(run_script.router, prefix="/api")
 app.include_router(run_script_log.router, prefix="/api")
 
+# Solo in sviluppo
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
