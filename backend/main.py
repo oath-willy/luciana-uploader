0ed9f4da34e6959dd945f0ca887fbad67bc316fa
Wilson Sgroi <wilson.sgroi@keystone.local>
2025-12-10 13:10:09 +0100

    main.py api.db

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 from api.routes import router
 from api import run_script
 from api import run_script_log
-from api import products
+from backend.api.db import products
 
 load_dotenv()
 
